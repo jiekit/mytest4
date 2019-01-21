d63d3b73e35f41d70960b9b602759bc46084b3cd
jie <123>
2019-01-21 17:39:04 +0800

    提交

---
+++ git_day01_demo/src/main/java/com/itheima/domain/Men.java
@@ -0,0 +1,12 @@
+package com.itheima.domain;
+
+/**
+ * @program: git_day01_demo
+ * @description: TODO
+ * @author: Mr.Jie
+ * @create: 2019-01-21 17:38
+ **/
+
+public class Men {
+    private String name;
+}
